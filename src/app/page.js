2364063bb32e5b1e6fa49c7ac65add21690b3e0d
giacomotolari <tolarigiacomo@gmail.com>
2023-08-02 15:59:43 +0200

    update engines and disable posts and postform

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,8 @@ export default async function Home() {
   return (
     <main className="p-10 max-w-3xl mx-auto">
       <h1 className="text-3xl font-bold mb-4">Anonymous posts</h1>
-      <PostForm action={formAction.create} />
-      <Posts />
+      {/* <PostForm action={formAction.create} />
+      <Posts /> */}
     </main>
   );
 }
