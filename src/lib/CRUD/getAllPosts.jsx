7854346123ac4fbdd8768d9d835ce5ef87cf85b1
giacomotolari <tolarigiacomo@gmail.com>
2023-08-03 09:06:01 +0200

    use NEXT_PUBLIC_BACKEND_URL

--- src/lib/CRUD/getAllPosts.jsx
+++ src/lib/CRUD/getAllPosts.jsx
@@ -1,11 +1,13 @@
 export default async function getAllPosts() {
-    const response = await fetch(`${process.env.BACKEND_URL}/api/posts`, {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/posts`,
+    {
       method: "GET",
       cache: "no-store", // avoid static data cache because we want to see new posts if added,
-    });
-  
-    if (!response.ok) throw new Error("failed to fetch the posts");
-  
-    return await response.json();
-  }
-  
+    }
+  );
+
+  if (!response.ok) throw new Error("failed to fetch the posts");
+
+  return await response.json();
+}
--- src/lib/CRUD/getPost.jsx
+++ src/lib/CRUD/getPost.jsx
@@ -1,8 +1,11 @@
-export default async function getPost(postId ) {
-  const response = await fetch(`${process.env.BACKEND_URL}/api/posts/${postId}`, {
-    method: "GET",
-    cache: "no-store", // avoid static data cache because we want to see new posts if added,
-  });
+export default async function getPost(postId) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/posts/${postId}`,
+    {
+      method: "GET",
+      cache: "no-store", // avoid static data cache because we want to see new posts if added,
+    }
+  );
   if (!response.ok) throw new Error("failed to fetch the post");
 
   return await response.json();
