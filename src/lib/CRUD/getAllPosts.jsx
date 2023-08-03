6109970e499676cadce6eb5c76cd7262ef608f5e
giacomotolari <tolarigiacomo@gmail.com>
2023-08-03 12:14:00 +0200

    update with UX improvement

--- src/lib/CRUD/getAllPosts.jsx
+++ src/lib/CRUD/getAllPosts.jsx
@@ -6,12 +6,10 @@ export default async function getAllPosts() {
       method: "GET",
       cache: "no-store", // avoid static data cache because we want to see new posts if added,
     });
-    console.log("🚀 ~ file: getAllPosts.jsx:7 ~ getAllPosts ~ response:", response)
 
     if (!response.ok) {
       throw new Error("Failed to fetch the posts");
     }
-
 
     return await response.json();
   } catch (error) {
@@ -20,4 +18,3 @@ export default async function getAllPosts() {
     throw error; // Rethrow the error to propagate it to the caller
   }
 }
-
